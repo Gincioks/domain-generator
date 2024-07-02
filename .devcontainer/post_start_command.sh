c24bfed7f8954e0bc4f075369a4a22c50939187d
Gince <ruzauskas.gintaras@gmail.com>
2024-07-02 12:12:23 +0300

    chore: Activate devcontainer scripts and update env defaults

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cp .env.example .env
-
-cd /workspaces/domain-generator/api && uvicorn main:app &
-cd /workspaces/domain-generator/web && bun dev &
-
 echo -e "\033[1;32mUse the following commands to start the services:\033[0m"
 echo -e "\033[1;34m  start-api\033[0m \033[1;32m- Start the backend\033[0m"
 echo -e "\033[1;34m  start-web\033[0m \033[1;32m- Start the frontend\033[0m"
